5468ad172b18a27146686370a388d5f5eefd75a2
Dauda Ahmed Afolabi <55094632+dev-afolabi@users.noreply.github.com>
2021-09-13 13:47:50 +0100

    moved swagger

--- Startup.cs
+++ Startup.cs
@@ -53,11 +53,11 @@ namespace FletcherProj
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FletcherProj v1"));
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FletcherProj v1"));
             app.UseHttpsRedirection();
+
 
             app.UseRouting();
 
